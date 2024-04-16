25a1233d94542850eb6f929ab5abbed582702437
Comfort104 <162302841+Comfort104@users.noreply.github.com>
2024-04-16 10:44:49 +0200

    Add files via upload

---
+++ Icetask.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package icetask;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Icetask {
+    public static void main(String[] args) {
+        // Fill in user details
+        String name = "Comfort Emo";
+        String email = "tkemo25@gmail.com";
+        String number = "0697666198";
+
+        // Display user details
+        System.out.println("FullName" + name);
+        System.out.println("emailAddress" + email );
+        System.out.println("phoneNumber" +  number);
+    }
+  }
+
